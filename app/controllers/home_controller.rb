586ce04e0dc0ae58015f144368a245f6079810cf
davidmao <damagoza@gmail.com>
2018-06-19 22:53:40 +0000

    Form tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @desarrollo_cuotas = []
+    params[:amount].to_i.times do |time|
+      line = {}
+      line["mes"] = (time.to_i + 1)
+      line["saldo"] = (time.to_i + 1)
+      line["amortizacion"] = (time.to_i + 1)
+      @desarrollo_cuotas.push(line)
+    end
+    puts @desarrollo_cuotas
   end
 end
