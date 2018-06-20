e97d16726545b99e68d65014efda5c8686a1070e
davidmao <damagoza@gmail.com>
2018-06-20 03:08:02 +0000

    Calculo de las cuotas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,35 @@
 class HomeController < ApplicationController
   def index
-    @desarrollo_cuotas = []
-    params[:amount].to_i.times do |time|
-      line = {}
-      line["mes"] = (time.to_i + 1)
-      line["saldo"] = (time.to_i + 1)
-      line["amortizacion"] = (time.to_i + 1)
-      @desarrollo_cuotas.push(line)
+    @quota_development = []
+    @amount = params["amount"].to_f
+    @term = params["term"].to_i
+    @interest = params["interest"].to_f
+    @amortization = @amount/@term
+    @total = 0
+      @term.times do |time|
+      if @amount > 0
+        line = {}
+        line["month"] = (time + 1)
+        if time > 0
+          if @amount > @amortization
+            @amount -= @amortization
+          else
+            @amount -= @amount
+          end
+        else
+          
+        end
+        @interests = @amount * (@interest/100)      
+        @quota_month = @amortization + @interests
+        line["amount"] = @amount  
+        line["balance_final"] = @amount         
+        line["amortization"] = @amortization
+        line["interests"] = @interests
+        line["quota_month"] = @quota_month
+        @total += @quota_month
+      end
+      @quota_development.push(line)
     end
-    puts @desarrollo_cuotas
+    puts @quota_development
   end
 end
